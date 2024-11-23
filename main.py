54ea7511f32ed11ea4efc3b1f9242618be2a4481
AndrewDeng3 <andrewyxd19@gmail.com>
2024-11-23 13:32:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -158,4 +158,4 @@ async def on_message(message):
     await message.channel.send(gpt)
 
 #keep_alive()
-client.run("MTIxMDU5OTA3NzQwNzEwMDk4OQ.Gd-H7w.uNAYGU9PE0dw0emLNgj00MqqcpEMa7FUTaClzw")
+client.run("MTIxMDU5OTA3NzQwNzEwMDk4OQ.GNt4gA.pcamfk4TZt6kXgfDBH4bpJYdcrrwTSESSbxk1s")
